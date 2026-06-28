985098dff564b7bdf91c9f30cc2c41294a1b7909
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-06-28 10:19:42 +0530

    final change

--- 01_background_changer/script.js
+++ 01_background_changer/script.js
@@ -1,10 +1,13 @@
 const buttons = document.querySelectorAll('.button')
+const clickSound = new Audio("../02_guess_number/click.mp3");
 
 buttons.forEach((button) => {
     
     button.addEventListener("click", (e) =>{
-    const body = document.querySelector("body");
-    body.style.backgroundColor = e.target.id
+        clickSound.currentTime = 0;
+        clickSound.play();
+        const body = document.querySelector("body");
+        body.style.backgroundColor = e.target.id
     })
     
 });
